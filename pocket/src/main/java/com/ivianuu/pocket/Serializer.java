28a87a7e18a2f56257240c89ed031ce5d9e4c228
IVIanuu <IVIanuu@gmail.com>
2017-09-09 01:49:42 +0200

    Added jackson serializer

--- pocket/src/main/java/com/ivianuu/pocket/Serializer.java
+++ pocket/src/main/java/com/ivianuu/pocket/Serializer.java
@@ -29,7 +29,7 @@ public interface Serializer {
      * Serializes the value
      */
     @NonNull
-    <T> String serialize(@NonNull T value);
+    <T> String serialize(@NonNull T value) throws Exception;
 
     /**
      * Deserializes the string to the value
